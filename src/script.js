1c2411d3800346186f42cf35a0b10dc441425bea
Gabriel Pfeffer <gabrielpfeffer@gmail.com>
2019-10-23 00:24:56 +0200

    nuevo formato comenzado

--- src/script.js
+++ src/script.js
@@ -1,9 +1,6 @@
 var botonMenu = document.querySelector('.nav__btn');
-var mostrarMenu=document.querySelector('header');
+var mostrarMenu = document.querySelector('header');
 
 botonMenu.addEventListener('click', function(){
     mostrarMenu.classList.toggle('headerAmpliado');
 });
- botonMenu.addEventListener('click', function(event){
-     console.log(event);
- })
