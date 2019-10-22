82c4429e2f093f85d4b9385d82ba6c11f4d89541
Gabriel Pfeffer <gabrielpfeffer@gmail.com>
2019-10-23 01:50:59 +0200

    set main files

--- src/script.js
+++ src/script.js
@@ -1,6 +1,25 @@
-var botonMenu = document.querySelector('.nav__btn');
-var mostrarMenu = document.querySelector('header');
+const menuBtn = document.querySelector('.nav_btn');
+const menu = document.querySelector('nav');
+const topOfMenu = menu.offsetTop;
 
-botonMenu.addEventListener('click', function(){
-    mostrarMenu.classList.toggle('headerAmpliado');
+function trancarNav() {
+    if(window.scrollY >= topOfMenu) {
+        document.body.style.paddingTop = menu.offsetHeight + 'px';
+        menu.classList.add('fixed-nav');
+    } else {
+        document.body.style.paddingTop = 0;
+        menu.classList.remove('fixed-nav');
+    }
+};
+
+
+
+
+
+
+
+menuBtn.addEventListener('click', function(){
+    menu.classList.toggle('visible');
 });
+
+window.addEventListener('scroll', trancarNav);
